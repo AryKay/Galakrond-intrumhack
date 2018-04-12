ec992d1123628b762e1f5bdff02e5decf17d3c3c
Igor Vileikis <festys.rpo@gmail.com>
2018-04-12 13:53:47 +0300

    updated home screen

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -6,48 +6,55 @@ import { Images } from "../Themes";
 // Styles
 import styles from "./Styles/LaunchScreenStyles";
 
-// Mock Goal Data
-// TODO: Change component in ItemCard
-const cards = [
-  {
-    id: "0",
-    title: "Macbook Air",
-    picture: require("../Images/ir.png"),
-    content: <Text>Macbook Air</Text>
-  },
-  {
-    id: "1",
-    title: "Barcelona Trip",
-    picture: require("../Images/ir.png"),
-    content: <Text>Barcelona Trip</Text>
-  },
-  {
-    id: "2",
-    title: "Gucci Bag",
-    picture: require("../Images/ir.png"),
-    content: <Text>Gucci Bag</Text>
-  }
-];
-
 // TODO: create buttons, replace header components with custom ones
 export default class HomeScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
 
     return (
-      <View style={styles.mainContainer}>
+      <View
+        style={[styles.mainContainer, { flex: 1, backgroundColor: "#485563" }]}
+      >
         <Header
           // leftComponent={{ icon: "menu", color: "#fff" }}
-          centerComponent={{ text: "Home", style: { color: "#fff" } }}
+          centerComponent={{ text: "Daily Budget", style: { color: "#fff" } }}
           // rightComponent={{ icon: "home", color: "#fff" }}
-          outerContainerStyles={{ backgroundColor: "#000000" }}
+          outerContainerStyles={{ backgroundColor: "#222A33" }}
         />
-        {/* <Button
-          title="Go to morning"
-          onPress={() => navigate("MorningScreen", { name: "Jane" })}
-        />
-        <StatusBar barStyle="light-content" hidden={false} />
-        <CardList cards={cards} /> */}
+        <ScrollView
+          contentContainerStyle={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 72 }}>55.5€</Text>
+          <Text style={{ color: "#B2B4B7", fontSize: 18 }}>OUT OF 100€</Text>
+        </ScrollView>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "flex-end",
+            marginVertical: 10,
+            marginHorizontal: 10,
+          }}
+        >
+          <View>
+            <Text style={{ color: "#2CC197", fontSize: 28 }}>2,540€</Text>
+            <Text style={{ color: "#B2B4B7", fontSize: 16 }}>BALANCE</Text>
+          </View>
+
+          <View>
+            <Text style={{ color: "#FF638F", fontSize: 28 }}>350€</Text>
+            <Text style={{ color: "#B2B4B7", fontSize: 16 }}>
+              INVESTMENTS
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
